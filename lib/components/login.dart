4b94e592daa3341953e2a9fa2a26d5cfc5207c0b
Hicham B <hicham.b@digital-dynamics.ma>
2021-07-16 11:20:24 +0100

    Problem routes

--- lib/components/login.dart
+++ lib/components/login.dart
@@ -4,189 +4,213 @@ import '../main.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
-  Login({Key? key}) : super(key: key);
+  Login({Key? key, required this.loginCallBack}) : super(key: key);
+  final Function loginCallBack;
 
   @override
-  _LoginState createState() => _LoginState();
+  _LoginState createState() => _LoginState(loginCallBack);
 }
 
 class _LoginState extends State<Login> {
-  _LoginState();
+  Function loginCallBack;
+  String _username = "", _password = "";
+
+  _LoginState(this.loginCallBack);
+
+  validationLogin(_username, _password) {
+    loginCallBack(_username, _password);
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    final formKey = new GlobalKey<FormState>();
     return Scaffold(
       backgroundColor: Color(0xFFfff3e5),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 150,
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Image.asset("assets/images/login-titre-left.png"),
-                  ),
-                  Text(
-                    'Login',
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFFcf4c30)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Image.asset("assets/images/login-titre-right.png"),
-                  )
-                ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 150,
               ),
-            ),
-            SizedBox(
-              height: 70,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              height: 54,
-              width: 300,
-              decoration: BoxDecoration(
-                color: Color(0xFFfff3e5),
-                border: Border.all(width: 3, color: Colors.black),
-                borderRadius: BorderRadius.circular(50),
-              ),
-              child: Row(
-                children: <Widget>[
-                  SizedBox(width: 10),
-                  Icon(
-                    Icons.person_rounded,
-                    color: Colors.grey.withOpacity(0.5),
-                  ),
-                  SizedBox(width: 15),
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {
-                        print(value);
-                      },
-                      decoration: InputDecoration(
-                        hintText: "Nom d'utilisteur",
-                        hintStyle: TextStyle(
-                          color: Colors.grey.withOpacity(0.5),
-                        ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                      ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Image.asset("assets/images/login-titre-left.png"),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              height: 54,
-              width: 300,
-              decoration: BoxDecoration(
-                color: Color(0xFFfff3e5),
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(width: 3, color: Colors.black),
-              ),
-              child: Row(
-                children: <Widget>[
-                  SizedBox(width: 10),
-                  Icon(
-                    Icons.lock_outline,
-                    color: Colors.grey.withOpacity(0.5),
-                  ),
-                  SizedBox(width: 15),
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {
-                        print(value);
-                      },
-                      decoration: InputDecoration(
-                        hintText: "Password",
-                        hintStyle: TextStyle(
-                          color: Colors.grey.withOpacity(0.5),
-                        ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                      ),
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFcf4c30)),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 70,
-            ),
-            MaterialButton(
-              height: 45,
-              minWidth: 150,
-              shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(5)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Home()),
-                );
-              },
-              child: Text(
-                "Se Connecter",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.white,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Image.asset("assets/images/login-titre-right.png"),
+                    )
+                  ],
                 ),
               ),
-              color: Color(0xFFcf4c30),
-            ),
-            SizedBox(
-              height: 70,
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Image.asset("assets/images/google-icon.png"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Image.asset("assets/images/facebook-icon.png"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Image.asset("assets/images/twitter-icon.png"),
-                  ),
-                ],
+              SizedBox(
+                height: 70,
               ),
-            ),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Register()),
-                  );
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                height: 54,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Color(0xFFfff3e5),
+                  border: Border.all(width: 3, color: Colors.black),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(width: 10),
+                    Icon(
+                      Icons.person_rounded,
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: TextFormField(
+                        onChanged: (value) {
+                          print(value);
+                          _username = value.toString();
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Nom d'utilisteur",
+                          hintStyle: TextStyle(
+                            color: Colors.grey.withOpacity(0.5),
+                          ),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                height: 54,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Color(0xFFfff3e5),
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(width: 3, color: Colors.black),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(width: 10),
+                    Icon(
+                      Icons.lock_outline,
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          print(value);
+                          _password = value.toString();
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          hintStyle: TextStyle(
+                            color: Colors.grey.withOpacity(0.5),
+                          ),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              MaterialButton(
+                height: 45,
+                minWidth: 150,
+                shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(5)),
+                onPressed: () {
+                  validationLogin(_username, _password);
                 },
                 child: Text(
-                  'Creér un Compte',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  "Se Connecter",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ),
+                color: Color(0xFFcf4c30),
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: Image.asset("assets/images/google-icon.png"),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: Image.asset("assets/images/facebook-icon.png"),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: Image.asset("assets/images/twitter-icon.png"),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Register()),
+                    );
+                  },
+                  child: Text(
+                    'Creér un Compte',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 import 'components/login.dart';
+import 'main.dart';
 
 void main() {
   runApp(MaterialApp(home: SplashScreen()));
@@ -16,12 +18,52 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  void initState() {
-    super.initState();
+  String _username = "", _password = "";
+
+  validationLogin(_username, _password) {
+    print('hello baby');
+    print(_username.toString());
+    print(_password.toString());
+    if (_username.toString() == "Hicham" &&
+        _password.toString() == "Berdouki") {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    } else {
+      showDialog(
+          context: context,
+          builder: (_) {
+            return AlertDialog(
+              title: Text('Warning'),
+              content: Text("Mot de passe Erronée"),
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  // passing false
+                  child: Text('Ok'),
+                ),
+              ],
+            );
+          });
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    
     Timer(
-        Duration(seconds: 3),
-        () => Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (BuildContext context) => Login())));
+      Duration(seconds: 3),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (BuildContext context) => Login(
+            loginCallBack: validationLogin(_username, _password),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
